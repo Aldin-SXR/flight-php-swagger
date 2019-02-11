08eec6ad3426957aa51206f9f1b7c5ae7f95d5a1
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-11 15:36:55 +0100

    Basic logger test.

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-require_once __DIR__."/vendor/autoload.php";
-
 require_once __DIR__."/utils/Logger.php";
 
 Flight::register("logger", "Logger", [ "logs", "debug.log" ]);
 
-
-Flight::route("POST /test", function() {
-    Flight::json([ "meow" => "meow" ]);
-    Flight::logger()->log(Flight::request(), Flight::response());
+/* Flight middleware | Logging */
+Flight::after("start", function(&$params, &$output) {
+    if (Flight::request()->url !== "/") {
+        Flight::logger()->log(Flight::request(), Flight::response());
+    }
 });
-
-Flight::start();
