417e88cf7678126ae35f367b9aa8477eb69447cf
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-13 17:05:11 +0100

    Fixed Config file.

--- config/Config.sample.php
+++ config/Config.sample.php
@@ -20,11 +20,14 @@ define("DOCS_ANNOTATION_LOCATIONS", [
     __DIR__."/../docs/doc_setup.php"
 ]);
 
-define("API_BASE_PATH", explode("/", dirname(dirname(__FILE__)))[count(explode("/", dirname(dirname(__FILE__)))) - 1]); // path to your server/API
+/* Project definitions */
+define("API_BASE_PATH", "http://localhost/".explode("/", dirname(dirname(__FILE__)))[count(explode("/", dirname(dirname(__FILE__)))) - 1]); // path to your server/API
 define("PROJECT_TITLE", "Flight/Swagger bundle"); // project title
 define("PROJECT_DESCRIPTION", "FlightPHP micro-framework bundled with Swagger and other useful utilities."); // project description
 define("PROJECT_VERSION", "0.1"); // project version
+define("PROJECT_DOCS_TITLE", "FlightPHP/Swagger bundle"); // title of the HTML page for the generated documentation 
 
+/* Author/team definitions */
 define("AUTHOR_NAME", "Aldin Kovačević");
 define("AUTHOR_EMAIL", "aldin@tribeos.io");
 define("AUTHOR_URL", "https://www.linkedin.com/in/aldin-kovacevic/");
