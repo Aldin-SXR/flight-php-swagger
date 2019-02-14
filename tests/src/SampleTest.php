ce68b821a18ac5296cb1e76cb147261487291edc
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-14 11:26:26 +0100

    Added tests/ suite to project skeleton.

---
+++ tests/src/SampleTest.php
@@ -0,0 +1,57 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__."/../../app/utils/ModelValidator.php";
+
+/**
+ * Sample test class which showcases some basic aspects of PHPUnit.
+ */
+class SampleTest extends TestCase {
+    private $test_var;
+
+    /**
+     * Set up a test case.
+     * @return void
+     */
+    public function setUp() {
+        $this->test_var = "This is a test variable.";
+    }
+
+    /**
+     * A basic test case example (positive).
+     * @return void
+     */
+    public function testTestSuiteIsWorkingForPositiveAssertions() {
+        /* Sample model */
+        $sample_model = [
+            "sample_attribute_1" => "The answer to life, the universe and everything?",
+            "sample_attribute_2" => 42
+        ];
+        /* Assertions */
+        $this->assertEquals("This is a test variable.", $this->test_var);
+        $this->assertCount(2, $sample_model); 
+        $this->assertNull(ModelValidator::validate_model(SampleModel::class, $sample_model));
+        $this->assertTrue(true);
+    }
+
+        /**
+     * A basic test case example (negative).
+     * @return void
+     */
+    public function testTestSuiteIsWorkingForNegativeAssertions() {
+        /* Sample model */
+        $sample_model = [
+            "sample_attribute_1" => "This sample model is invalid."
+        ];
+        /* Assertions */
+        $this->assertNotNull(ModelValidator::validate_model(SampleModel::class, $sample_model));
+        $this->assertNotTrue(false);
+    }
+
+    /**
+     * Tear down a test case.
+     * @return void
+     */
+    public function tearDown() {
+        $this->test_var = NULL;
+    }
+}
