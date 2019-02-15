0cb0261dc40b31c6a7ba5bac1fe6c40ccd152414
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-15 10:51:38 +0100

    Fixed logger format.

--- app/utils/Logger.php
+++ app/utils/Logger.php
@@ -21,16 +21,50 @@ class Logger {
     }
 
     public function log($request, $response) {
+        ob_end_flush();
         $log = [
             "date" => date('Y-m-d H:i:s'),
             "client_ip" => $request->ip,
             "method" => $request->method,
             "route" => $request->url,
-            "query_params" => $request->query->getData(),
-            "request_body" => $request->data->getData(),
-            "response" => $this->process_response($response)
+            "query_params" => json_encode($request->query->getData()),
+            "request_body" => json_encode($request->data->getData()),
+            "response" => json_encode($this->process_response($response)),
+            "request_headers" => json_encode(getallheaders()),
+            "response_headers" => json_encode(apache_response_headers())
         ];
+        $log = implode("\t", $log)."\n";
         
-        file_put_contents($this->file_name, print_r($log, true)."=================================\n", FILE_APPEND);
+        file_put_contents($this->file_name, print_r($log, true), FILE_APPEND);
+    }
+}
+
+/**
+ * Replacement functions for Nginx setups.
+ */
+
+ /* Get all request headers */
+if (!function_exists("getallheaders")) {
+    function getallheaders() {
+        $headers = []; 
+        foreach ($_SERVER as $name => $value) { 
+            if (substr($name, 0, 5) == 'HTTP_')  { 
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+            } 
+        } 
+        return $headers; 
+    }
+}
+
+/* Get all response headers */
+if (!function_exists('apache_response_headers')) {
+    function apache_response_headers_2 () {
+        $arh = array();
+        $headers = headers_list();
+        foreach ($headers as $header) {
+            $header = explode(":", $header);
+            $arh[array_shift($header)] = trim(implode(":", $header));
+        }
+        return $arh;
     }
 }
