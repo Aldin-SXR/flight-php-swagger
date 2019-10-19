78504590a66fec167314ac8799a203018c7f26cc
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-10-20 01:03:37 +0200

    Updated Swagger options and favicon.

--- docs/index.php
+++ docs/index.php
@@ -1,63 +1,64 @@
-<?php 
-	/* Project configuration */
-  require_once __DIR__."/../config/Config.php"; 
+<?php
+/* Project configuration */
+require_once __DIR__ . '/../config/config.php';
 ?>
 
 <!-- HTML for static distribution bundle build -->
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <title><?php echo PROJECT_DOCS_TITLE ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/swagger-ui.css" >
-    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/tribeos.css" >
+    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/swagger-ui.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/custom-swagger.css">
     <link rel="icon" type="image/png" href="<?php echo DOCS_FOLDER ?>/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="<?php echo DOCS_FOLDER ?>/favicon-16x16.png" sizes="16x16" />
     <style>
-      html
-      {
-        box-sizing: border-box;
-        overflow: -moz-scrollbars-vertical;
-        overflow-y: scroll;
-      }
-      *,
-      *:before,
-      *:after
-      {
-        box-sizing: inherit;
-      }
-      body
-      {
-        margin:0;
-        background: #fafafa;
-      }
-    </style>
-  </head>
+        html {
+            box-sizing: border-box;
+            overflow: -moz-scrollbars-vertical;
+            overflow-y: scroll;
+        }
 
-  <body>
+        *,
+        *:before,
+        *:after {
+            box-sizing: inherit;
+        }
+
+        body {
+            margin: 0;
+            background: #fafafa;
+        }
+    </style>
+</head>
+
+<body>
     <div id="swagger-ui"></div>
 
     <script src="<?php echo DOCS_FOLDER ?>/swagger-ui-bundle.js"> </script>
     <script src="<?php echo DOCS_FOLDER ?>/swagger-ui-standalone-preset.js"> </script>
     <script>
-    window.onload = function() {      
-      // Begin Swagger UI call region
-      const ui = SwaggerUIBundle({
-        url: "<?php echo DOCS_FOLDER ?>/swagger.json",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        plugins: [
-          SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "StandaloneLayout"
-      })
-      // End Swagger UI call region
-      window.ui = ui
-    }
-  </script>
-  </body>
+        window.onload = function() {
+            // Begin Swagger UI call region
+            const ui = SwaggerUIBundle({
+                url: "<?php echo DOCS_FOLDER ?>/swagger.json",
+                dom_id: '#swagger-ui',
+                deepLinking: true,
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
+                plugins: [
+                    SwaggerUIBundle.plugins.DownloadUrl
+                ],
+                layout: "StandaloneLayout"
+            })
+            // End Swagger UI call region
+            window.ui = ui
+        }
+    </script>
+</body>
+
 </html>
--- docs/swagger.php
+++ docs/swagger.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-require_once (__DIR__."/../config/Config.php");
+require_once (__DIR__."/../config/config.php");
 
 error_reporting(1);
 ini_set('display_errors', 1);
@@ -11,13 +11,14 @@ ini_set('display_errors', 1);
  * Add the folders containing the specification annotations to the \OpenApi\scan array.
  * By default, routes/ and models/ folders are included, as well as the doc_setup.php setup file.
  */
-$arr = explode("/", $_GET['send']);
-if ($arr[0] == 'swagger.json') {
-    define('SERVER_ROOT', realpath(dirname(__FILE__)));
-    require_once SERVER_ROOT . "/../vendor/autoload.php";
-    $openapi = \OpenApi\scan(DOCS_ANNOTATION_LOCATIONS);
-    $openapi->servers[0]->url = API_BASE_PATH;
+if (array_key_exists('send', $_GET)) {
+    $arr = explode("/", $_GET['send']);
+    if ($arr[0] == 'swagger.json') {
+        require_once SERVER_ROOT . "/vendor/autoload.php";
+        $openapi = \OpenApi\scan(DOCS_ANNOTATION_LOCATIONS);
+        $openapi->servers[0]->url = str_replace('docs/swagger.json', '', API_BASE_PATH);
 
-    header('Content-Type: application/json');
-    echo $openapi->toJson();
-}        
+        header('Content-Type: application/json');
+        echo $openapi->toJson();
+    }        
+}
